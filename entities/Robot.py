92326933488c4a79657e829675cbd0828be03510
Carlos Matos <carlosmatos@estudante.ufscar.br>
2024-04-29 19:52:55 -0300

    Update Robot.py
    Mais uma vez arrumei as variáveis

--- entities/Robot.py
+++ entities/Robot.py
@@ -6,24 +6,25 @@ class Robot(KinematicBody):
         super().__init__()
         self.robot_id = robot_id
         self.actuator = actuator
-        #self.topRightMotor = 0
-        #self.bottomRightMotor = 0
-        #self.topLeftMotor = 0
-        #self.bottomLeftMotor = 0
+        #self.top_right_motor = 0
+        #self.bottom_right_motor = 0
+        #self.top_left_motor = 0
+        #self.bottom_left_motor = 0
         #self.obst = Obstacle(self)
         #self.target = Target()
-        self.vbottomRight = 0  # Velocidade do motor inferior direito
-        self.vbottomLeft = 0   # Velocidade do motor inferior esquerdo
-        self.vtopRight = 0     # Velocidade do motor superior direito
-        self.vtopLeft = 0      # Velocidade do motor superior esquerdo
+        self.v_bottom_right = 0  # Velocidade do motor inferior direito
+        self.v_bottom_left = 0   # Velocidade do motor inferior esquerdo
+        self.v_top_right = 0     # Velocidade do motor superior direito
+        self.v_top_left = 0      # Velocidade do motor superior esquerdo
         
 
-    def sim_set_vel(self, vtopRight, vtopLeft, vbottomRight, vbottomLeft):
-        self.vtopRight = vtopRight
-        self.vtopLeft = vtopLeft
-        self.vbottomRight = vbottomRight
-        self.vbottomLeft = vbottomLeft
+    def sim_set_vel(self, v_top_right, v_top_left, v_bottom_right, v_bottom_left):
+        self.v_top_right = v_top_left
+        self.v_top_left = v_top_right
+        self.v_bottom_right = v_bottom_right
+        self.v_bottom_left = v_bottom_left
 
-        self.set_velocities(0, 0, self.vtopRight, self.vtopLeft, self.vbottomRight, self.vbottomLeft)
-        self.actuator.send_wheelVelocity_message(self.robot_id, self.vtopRight, self.vtopLeft, self.vbottomRight, self.vbottomLeft)
+        self.set_velocities(0, 0, self.v_top_right, self.v_top_left, self.v_bottom_right, self.v_bottom_left)
+        self.actuator.send_wheelVelocity_message(self.robot_id, self.v_top_right, self.v_top_left, self.v_bottom_right, self.v_bottom_left)
+    
     
