48288c0e43f377983335c4adc17e1deec12308d5
Carlos Matos <carlosmatos@estudante.ufscar.br>
2024-07-21 14:00:37 -0300

    Melhorias no fluxo do código

--- entities/Robot.py
+++ entities/Robot.py
@@ -2,6 +2,8 @@ from entities.KinematicBody import KinematicBody
 from communication.actuator import Actuator
 from entities.Target import Target
 from entities.Obstacle import Obstacle
+from control.PID import PID
+from control.PID_discrete import PID_discrete
 
 import numpy as np
 
@@ -17,14 +19,27 @@ class Robot(KinematicBody):
         # self.bottom_right_motor = 0
         # self.top_left_motor = 0
         # self.bottom_left_motor = 0
-        # self.obst = Obstacle(self)
+        self.obst = Obstacle(self)
         self.cont_target = 0  # Adicionado cont_target como atributo do robô
         self.target = Target()
-        self.obst = Obstacle(self)
         self.v_bottom_right = 0  # Velocidade do motor inferior direito
         self.v_bottom_left = 0  # Velocidade do motor inferior esquerdo
         self.v_top_right = 0  # Velocidade do motor superior direito
         self.v_top_left = 0  # Velocidade do motor superior esquerdo
+        Kp_x = 6.551
+        Kd_x = 1.004
+        Ki_x = 0
+        N_x = 1 / 0.01898
+        Kp_y = 4.857
+        Kd_y = 1.077
+        Ki_y = 0
+        N_y = 1 / 0.01805
+        Kp_theta = 1.5
+        Kd_theta = 0
+        Ki_theta = 0
+        self.control_PID_x = PID_discrete(Kp_x, Kd_x, Ki_x, saturation=2, N=N_x)
+        self.control_PID_y = PID_discrete(Kp_y, Kd_y, Ki_y, saturation=2, N=N_y)
+        self.control_PID_theta = PID(Kp_theta, Kd_theta, Ki_theta, saturation=1)
 
     def sim_set_vel(self, v_top_right, v_top_left, v_bottom_right, v_bottom_left):
         self.v_top_right = v_top_left
@@ -57,12 +72,31 @@ class Robot(KinematicBody):
         self.target = target
 
     def target_reached(self):
-        # Implemente sua lógica para verificar se o robô alcançou o alvo aqui
         if self.target is None:
             return False
         current_position = np.array(
             [self.get_coordinates().X, self.get_coordinates().Y]
         )
-        target_position = np.array([self.target.x, self.target.y])
+        target_position = np.array(
+            [self.target.get_coordinates().X, self.target.get_coordinates().Y]
+        )
         distance_to_target = np.linalg.norm(current_position - target_position)
         return distance_to_target < 10
+
+    def set_robot_velocity(self, target_velocity_x, target_velocity_y, target_angular):
+        self.control_PID_x.set_target(target_velocity_x)
+        self.control_PID_y.set_target(target_velocity_y)
+        self.control_PID_theta.set_target(target_angular)
+
+        self.control_PID_x.set_actual_value(self.get_coordinates().X)
+        vx = self.control_PID_x.update()
+
+        self.control_PID_y.set_actual_value(self.get_coordinates().Y)
+        vy = self.control_PID_y.update()
+
+        self.control_PID_theta.set_actual_value(self.get_coordinates().rotation)
+        w = self.control_PID_theta.update_angular()
+
+        self.sim_set_global_vel(vx, vy, w)
+
+        return vx, vy, w
--- main.py
+++ main.py
@@ -3,24 +3,26 @@ from communication.actuator import Actuator
 from entities.Robot import Robot
 from path.visibilityGraph import VisibilityGraph
 from control.PID import PID
+from control.PID_discrete import PID_discrete
 from behavior.skills import *
-from path.desvio_obstaculo import *
 import time
 import numpy as np
 
 
-# Inicializar controladores PID
-pid = PID(Kp=1 / 100, Kd=-0.0631 / 100, Ki=0, saturation=2)
-pid.inicializar_controladores_PID()
-
 # Objeto de visão
 visao = Vision(ip="224.5.23.2", port=10020)
+
+# Visibilidade
+vg = VisibilityGraph()
 
 # Client de conexão de atuação do simulador
 actuator = Actuator(team_port=10301)
 
 # Robôs azuis (o primeiro é o robô controlado, os outros são obstáculos)
-robot0 = Robot(robot_id=0, actuator=actuator)
+robot0 = Robot(
+    robot_id=0,
+    actuator=actuator,
+)
 robot1 = Robot(robot_id=1, actuator=None)
 robot2 = Robot(robot_id=2, actuator=None)
 
@@ -33,16 +35,10 @@ enemy_robot1 = Robot(robot_id=1, actuator=None)
 enemy_robots = [enemy_robot0, enemy_robot1]
 
 
-# Visibilidade
-vg = VisibilityGraph()
-
-
 # Contador para garantir a leitura dos dados da câmera
 cont = 0
 cont_target = 0
 
-# Sinais de controle
-vx, vy, w = 0, 0, 0
 
 while True:
     t1 = time.time()
@@ -77,9 +73,9 @@ while True:
 
     # Se foi recebido ao menos 5 frames de visão, realizar o controle
     if cont == 5:
-        quadradinho(robot0, robots, enemy_robots, vg)
 
-        pid.setRobotVelocity(robot0)
+        go_to(robot0, 100, 0, robots, enemy_robots, vg)
+
         cont = 0
 
     t2 = time.time()
