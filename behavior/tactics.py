0dc3558f478c425571ee05f836bf90c12bb45f2f
JoaoP9735 <joaoandrade35@estudante.ufscar.br>
2024-08-24 16:34:35 -0300

    offense-basic(maraca)

--- behavior/tactics.py
+++ behavior/tactics.py
@@ -2,14 +2,14 @@ from skills import *
 
 def attack_behav(robot0, field):
     ball_coords = field.ball.get_coordinates()
-    if (800 < ball_coords.X <= 1000) & (200 <= ball_coords.Y <= 400):
+    if (400 < ball_coords.X <= 500) & (87.5 <= ball_coords.Y <= 222.5):
         go_to_point(robot0, 720, ball_coords.Y, field, target_theta=0)
     else:
-        va = (900-ball_coords.X, 340-ball_coords.Y)
-        vb = (900-ball_coords.X, 310-ball_coords.Y)
-        vc = (900-ball_coords.X, 300-ball_coords.Y)
-        vd = (900-ball_coords.X, 290-ball_coords.Y)
-        ve = (900-ball_coords.X, 260-ball_coords.Y)
+        va = (450-ball_coords.X, 180-ball_coords.Y)
+        vb = (450-ball_coords.X, 158-ball_coords.Y)
+        vc = (450-ball_coords.X, 150-ball_coords.Y)
+        vd = (450-ball_coords.X, 142-ball_coords.Y)
+        ve = (450-ball_coords.X, 120-ball_coords.Y)
         angle1 = math.angle_between(va,vc)
         angle2 = math.angle_between(vb,vc)
         angle3 = math.angle_between(vd,vc)
