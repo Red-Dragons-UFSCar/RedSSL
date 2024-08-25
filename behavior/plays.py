218d8f9de355d0297e8f58e962d8a8d10e93d158
Carlos Matos <carlosmatos@estudante.ufscar.br>
2024-08-25 17:48:05 -0300

    added coach

--- behavior/plays.py
+++ behavior/plays.py
@@ -1,4 +1,5 @@
 from behavior.tactics import goalie, zagueiro
+from behavior.skills import go_to_point
 
 
 def estrategia_basica(robot_goalie, robot_zagueiro, field):
@@ -8,3 +9,15 @@ def estrategia_basica(robot_goalie, robot_zagueiro, field):
     """
     goalie(robot_goalie, field)
     zagueiro(robot_zagueiro, field)
+
+
+def posicionar_robos(robot_goalie, robot_zagueiro, field):
+    """
+    Posiciona os robôs em pontos específicos quando o jogo está parado.
+    """
+    # Defina as posições específicas para os robôs quando o jogo está parado
+    print("Posicionando robô goleiro no ponto específico.")
+    go_to_point(robot_goalie, 30, 150, field, 0)
+
+    print("Posicionando robô zagueiro no ponto específico.")
+    go_to_point(robot_zagueiro, 150, 150, field, 0)
