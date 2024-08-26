e8662dff168d093e7528609f5c7b3964fb326ab4
KuririmFestozo <lkfestozo@estudante.ufscar.br>
2024-08-25 23:45:57 -0300

    Feat: reajuste do controlador para novo campo (adeus Kd/Ki!!)

--- entities/Robot.py
+++ entities/Robot.py
@@ -27,12 +27,12 @@ class Robot(KinematicBody):
 
         # Parâmetros PID
         Kp_x = 6.551
-        Kd_x = 3
-        Ki_x = 1.5
+        Kd_x = 0
+        Ki_x = 0
         N_x = 1 / 0.01898
         Kp_y = 4.857
-        Kd_y = 1.8
-        Ki_y = 1
+        Kd_y = 0
+        Ki_y = 0
         N_y = 1 / 0.01805
         Kp_theta = 5
         Kd_theta = 0
